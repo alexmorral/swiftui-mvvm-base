40f127fcfee62c19a66b10df47c1e3af440f8822
alexmorral <alexmorral@gmail.com>
2022-10-03 19:44:47 +0200

    Initial Commit

---
+++ SwiftUI_MVVM_Base/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUI_MVVM_Base
+//
+//  Created by Alex Morral on 3/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
